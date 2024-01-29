646e8a5382e3c036ed2ea9af7ad3df2d93cd17c6
DIMIBAGA <dianabarrantesga1510@gmail.com>
2024-01-29 15:29:03 -0500

    add all changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -18,7 +18,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/img4.jpg";
+import image from "../images/img4.png";
 const imageAltText = "Code";
 
 const Home = ({ name, title }) => {
