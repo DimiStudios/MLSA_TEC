07604b891806d8f6be1d656d8875126dc2ef8412
DIMIBAGA <dianabarrantesga1510@gmail.com>
2024-01-29 15:37:41 -0500

    add all change

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -18,7 +18,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/img4.png";
+import image from "../images/img2.jpg";
 const imageAltText = "Code";
 
 const Home = ({ name, title }) => {
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -17,7 +17,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/img2.jpg";
+import image from "../images/img4.png";
 const imageAltText = "desktop with books and laptop";
 /**
  * Project list
